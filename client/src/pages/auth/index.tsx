9d124268e4dc42929c541e0446518c301915c62b
DMould123 <95115610+DMould123@users.noreply.github.com>
2024-05-18 01:05:06 +0200

    Imported Clerk React components for authentication management, authentication buttons for signing up and signing out. Implemented routing for signed-in users using React Router Navigate

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,9 +1,30 @@
-const Auth = () => {
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton
+} from '@clerk/clerk-react'
+import { Navigate } from 'react-router-dom'
+
+export const Auth = () => {
   return (
-    <>
-      <h1>Auth</h1>
-    </>
+    <div className="sign-in-container">
+      <SignedOut>
+        <h1 style={{ display: 'flex', alignItems: 'center' }}>
+          <span style={{ marginRight: '10px' }}>Wedding Budget Planner</span>
+          <img
+            src="/wedding.png"
+            alt="Wedding Icon"
+            title="Wedding icons created by Freepik - Flaticon"
+            style={{ width: '50px', height: '50px' }}
+          />
+        </h1>
+        <SignUpButton mode="modal" />
+        <SignInButton mode="modal" />
+      </SignedOut>
+      <SignedIn>
+        <Navigate to="/" />
+      </SignedIn>
+    </div>
   )
 }
-
-export default Auth
