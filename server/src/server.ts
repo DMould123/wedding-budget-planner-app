af6adf2f93ccbd296caa120e2125a20260e2e676
DMould123 <95115610+DMould123@users.noreply.github.com>
2024-05-18 23:16:03 +0200

    Setup MongoDB cluster, install dependencies, and establish backend connection

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import express, { Express } from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Express = express()
+const port = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(cors())
+
+const mongoURI: string = process.env.MONGODB_URI || ''
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log('CONNECTED TO MONGODB!'))
+  .catch((err) => console.error('Failed to Connect to MongoDB:', err))
+
+app.listen(port, () => {
+  console.log(`Server Running on Port ${port}`)
+})
