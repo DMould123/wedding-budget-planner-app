1222244ecaa266b6cc013a070a10adc4b2ce0627
DMould123 <95115610+DMould123@users.noreply.github.com>
2024-05-21 01:37:20 +0200

    Added some basic css

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,6 +1,7 @@
 import { useUser } from '@clerk/clerk-react'
 import { WeddingExpenseForm } from './wedding-expense-form'
 import { WeddingExpenseList } from './wedding-expense-list'
+import './wedding-expense.css'
 
 export const Dashboard = () => {
   const { user } = useUser()
