210d37ba9fedeedc50bae440881f9e3232754e5b
DMould123 <95115610+DMould123@users.noreply.github.com>
2024-05-21 19:52:27 +0200

    Added  hook to calculate the total wedding budget based on expenses. Amount updates dynamically when expenses change

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -2,17 +2,27 @@ import { useUser } from '@clerk/clerk-react'
 import { WeddingExpenseForm } from './wedding-expense-form'
 import { WeddingExpenseList } from './wedding-expense-list'
 import './wedding-expense.css'
+import { useWeddingExpenses } from '../../context/wedding-expenses-context'
+import { useMemo } from 'react'
 
 export const Dashboard = () => {
   const { user } = useUser()
+  const { expenses } = useWeddingExpenses()
+
+  const weddingTotal = useMemo(() => {
+    let totalAmount = 0
+    expenses.forEach((expense) => {
+      totalAmount += expense.cost
+    })
+    return totalAmount
+  }, [expenses])
 
   return (
-    <>
-      <div className="dashboard-container">
-        <h1> Welcome {user?.firstName}! Here Are Your Wedding Finances:</h1>
-        <WeddingExpenseForm />
-        <WeddingExpenseList />
-      </div>
-    </>
+    <div className="dashboard-container">
+      <h1>Welcome {user?.firstName}! Here Are Your Wedding Finances:</h1>
+      <WeddingExpenseForm />
+      <div>Wedding Budget Left: {weddingTotal} SEK</div>
+      <WeddingExpenseList />
+    </div>
   )
 }
